e0afa97dbbd893567d6944c5710eaab21973c6a5
stefano.chiaudano <>
2021-06-11 16:11:56 +0200

    Tests for calibration

--- Arduino/BeeHive/BeeHive.ino
+++ Arduino/BeeHive/BeeHive.ino
@@ -1,37 +1,178 @@
-#include "DHT.h"
+// this library is used to write and read data from the EEPROM
+#include <EEPROM.h>
+
+// this library is used to define the Bluetotth serial
 #include "SoftwareSerial.h"
+
+// this library is used for the DHT temperature and humidity sensor
+#include "DHT.h"
+
+// this library is used for the HX711 module (scale sensor)
 #include "HX711.h"
 
+// defining the type of the DHT sensor: DHT11 or DHT22
 #define DHTTYPE DHT11
 
-static const int DHT_PIN = 4;
-
+// this variables are used to map the pins of the modules
 static const int BT_TX_PIN = 3;
 static const int BT_RX_PIN = 2;
+
+static const int DHT_PIN = 4;
 
 static const int HX_DT_PIN = 5;
 static const int HX_SCK_PIN = 6;
 
+// this valiable is used to set the number of samples used by the HX711 module
+static const int hx711Samples = 1;
+
+// Creating the Bluetooth serial instance
 SoftwareSerial bt = SoftwareSerial(BT_RX_PIN, BT_TX_PIN);
 
+// Creating the instance for the temperature and humidity module
+DHT dht(DHT_PIN, DHTTYPE);
+
+// Creating the instance for the scale sensor
 HX711 scale;
 
-float calibrationFactor = -29520;
+// This number is obtained during the scale calibration procedure
+float calibrationFactor = 26200;
 
+long zeroFactor = 0;
+
+// The name and the pin of the Bluetooth module
 String btName = "";
 String btPin = "";
 
-DHT dht(DHT_PIN, DHTTYPE);
-
-String leggiAmbiente() {
+// This method returns the themperature and the humidity
+void getEnvironment() {
   String ret = "T=";
-  ret.concat(dht.readTemperature());
+  float temp = dht.readTemperature();
+  float hum = dht.readHumidity();
+
+  if (isnan(temp) || isnan(hum)) {
+    bt.println("Error: temperature & humidity sensor faiulure");
+  }
+
+  ret.concat(temp);
   ret.concat(char(159));
   ret.concat("C, H=");
-  ret.concat(dht.readHumidity());
+  ret.concat(hum);
   ret.concat("%");
 
-  return ret;
+  bt.println(ret);
+}
+
+void getWeight() {
+  scale.power_up();
+  String ret = "T=";
+  ret.concat(String(scale.get_units(hx711Samples), 2));
+  ret.concat(" kg");
+
+  bt.println(ret);
+  scale.power_down();
+}
+
+void tareScale() {
+  scale.power_up();
+  scale.tare(hx711Samples);
+  zeroFactor = scale.read_average(hx711Samples);
+  // scale.set_offset(zeroFactor);
+  // EEPROM.put(0, zeroFactor);
+  bt.print("Scale tared. Zero factor: ");
+  bt.println(zeroFactor);
+  scale.power_down();
+}
+
+// checking for incoming commands vis Bluetooth
+void checkCommands() {
+  String txt = "";
+  if (bt.available()) {
+    txt = bt.readString();
+    char cmd = txt.charAt(0);
+    switch (cmd) {
+      // this command returns info
+      case 'i':
+        bt.println("Beehive 4.0 command list:");
+        bt.println("a -returns temperature, humidity and weight values");
+        bt.println("e - returns temperature, humidity values");
+        bt.println("w - returns the weight value");
+        bt.println("t - used to tare the scale");
+        bt.println("n {name} - used to change the Bluetooth name");
+        bt.println("p {pin} - change the Bluetooth pin");
+
+        break;
+        
+      // this command returns temperature, humidity and weight values
+      case 'a':
+        getEnvironment();
+        getWeight();
+
+        break;
+        
+      // this command returns temperature, humidity values
+      case 'e':
+        getEnvironment();
+        break;
+
+      // this command returns the weight value
+      case 'w':
+        getWeight();
+
+        break;
+
+      // this command is used to tare the scale
+      case 't':
+        tareScale();
+
+        break;
+
+      // this command is used to change the Bluetooth name
+      case 'n':
+        btName = txt.substring(2);
+        if (btName.length() < 2) {
+          bt.println("Error: Invlid Bluetooth name");
+          btName = "";
+        }
+
+        break;
+
+      // this command is used to change the Bluetooth pin
+      case 'p':
+        btPin = txt.substring(2);
+        if (btPin.length() < 4) {
+          bt.println("Error: Invalid Bluetooth Pin");
+          btPin = "";
+        }
+
+        break;
+
+      default:
+        bt.print("Error: Unknown command ");
+        bt.println(txt);
+
+        break;
+    }
+  }
+}
+
+// this method is used to change the Bluetooth name sending AT command to the Bluetooth module
+void changeBTName() {
+  bt.print("AT+BDNAME=");
+  bt.print(btName);
+  bt.write("\r\n");
+  bt.print("Changing Bluetooth name in: ");
+  bt.println(btName);
+  delay(5000);
+}
+
+// this method is used to change the Bluetooth pin sending AT command to the Bluetooth module
+void changeBTPin() {
+  bt.print("AT+BDPIN=");
+  bt.print(btPin);
+  bt.write("\r\n");
+  bt.print("Changing Bluetooth pin in: ");
+  bt.println(btPin);
+  delay(5000);
 }
 
 void setup() {
@@ -44,75 +185,22 @@ void setup() {
   bt.begin(115200);
 
   scale.begin(HX_DT_PIN, HX_SCK_PIN);
-
   scale.set_scale(calibrationFactor);
-  scale.tare();
+
+  // EEPROM.get(0, zeroFactor);
+  // scale.set_offset(zeroFactor);
+  scale.power_down();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  String txt = "";
-  if (bt.available()) {
-    txt = bt.readString();
-    char cmd = txt.charAt(0);
-    switch (cmd) {
-      case 'a':
-        bt.println(leggiAmbiente());
-        break;
-
-      case 'n':
-        btName = txt.substring(2);
-        if (btName.length() < 2) {
-          bt.println("Error. Nome non valido");
-          btName = "";
-        }
-
-        break;
-
-      case 'p':
-        btPin = txt.substring(2);
-        if (btPin.length() < 4) {
-          bt.println("Error. Pin non valido");
-          btPin = "";
-        }
-
-        break;
-
-      case 'w':
-        bt.print(scale.get_units(10), 1);
-        bt.println(" kg");
-
-        break;
-
-      case 't':
-        scale.tare();
-        bt.println("Bilancia azzerata");
-
-        break;
-
-      default:
-        bt.print("Error. Comando non riconosciuto:");
-        bt.println(txt);
-
-        break;
-    }
-  }
+  checkCommands();
 
   if (!btName.equals("")) {
-    bt.print("AT+BDNAME=");
-    bt.print(btName);
-    bt.write("\r\n");
-    bt.print("Sto cambiando il nome in:");
-    bt.println(btName);
-    delay(5000);
+    changeBTName();
   }
 
   if (!btPin.equals("")) {
-    bt.print("AT+BDPIN=");
-    bt.print(btPin);
-    bt.write("\r\n");
-    bt.print("Sto cambiando il pin in:");
-    bt.println(btPin);
-    delay(5000);
+    changeBTPin();
   }
 }
--- Arduino/HX711_Calibration/HX711_Calibration.ino
+++ Arduino/HX711_Calibration/HX711_Calibration.ino
@@ -1,23 +1,33 @@
 /*
 
- Arduino pin 
- 5 -> HX711 DT
- 6 -> HX711 SCK
- 5V -> HX711 VCC
- GND -> HX711 GND
- 
+  Arduino pin
+  5 -> HX711 DT
+  6 -> HX711 SCK
+  5V -> HX711 VCC
+  GND -> HX711 GND
+
 */
 
 #include "HX711.h"
+#include "SoftwareSerial.h"
 
 static const int HX_DT_PIN = 5;
 static const int HX_SCK_PIN = 6;
 
 HX711 scale;
 
-float calibrationFactor = -29220; //-29220 worked for my 200 kg max scale setup
+static const int BT_TX_PIN = 3;
+static const int BT_RX_PIN = 2;
+
+SoftwareSerial bt = SoftwareSerial(BT_RX_PIN, BT_TX_PIN);
+
+float calibrationFactor = 25900; //-29220 worked for my 200 kg max scale setup
+long zeroFactor;
+int samples = 10;
 
 void setup() {
+  bt.begin(115200);
+
   Serial.begin(9600);
   Serial.println("HX711 calibration");
   Serial.println("Remove all weight from scale");
@@ -25,34 +35,85 @@ void setup() {
   Serial.println("Press a to increase calibration factor");
   Serial.println("Press z to decrease calibration factor");
   Serial.println("Press t to tare the scale");
+  Serial.print("Samples: ");
+  Serial.println(samples);
 
   scale.begin(HX_DT_PIN, HX_SCK_PIN);
-  scale.set_scale();
-  scale.tare(); //Reset the scale to 0
+  // scale.power_up();
+  // scale.set_gain(128);
+  // delay(2000);
+  while (!scale.is_ready()) {
+  }
 
-  long zeroFactor = scale.read_average(); //Get a baseline reading
+  scale.set_scale();
+  //scale.set_offset(0);
+  scale.tare(samples); //Reset the scale to 0
+
+  zeroFactor = scale.read_average(samples); //Get a baseline reading
   Serial.print("Zero factor: "); //This can be used to remove the need to tare the scale. Useful in permanent scale projects.
   Serial.println(zeroFactor);
+
+  delay(5000);
+
+  scale.tare(samples); //Reset the scale to 0
+
+  delay(5000);
 }
 
 void loop() {
+  scale.set_scale(calibrationFactor); //Adjust to calibration factor
 
-  scale.set_scale(calibrationFactor); //Adjust to this calibration factor
+  float units = scale.get_units(samples);
+  float value = scale.get_value(samples);
 
-  Serial.print("Reading: ");
-  Serial.print(scale.get_units(), 2);
+  Serial.print("Reading ");
+  Serial.print("units: ");
+  Serial.print(units, 3);
   Serial.print(" kg");
-  Serial.print(" calibration_factor: ");
-  Serial.println(calibrationFactor);
+  Serial.print(", value: ");
+  Serial.print(value, 3);
+  //  Serial.print(", average: ");
+  //  Serial.print(scale.read_average(samples));
+  Serial.print(", calibration_factor: ");
+  Serial.print(calibrationFactor);
+  Serial.print(", zeroFactor: ");
+  Serial.println(zeroFactor);
 
-  if(Serial.available())
+  bt.print("units: ");
+  bt.print(units, 3);
+  bt.print(" kg");
+  bt.print(", value: ");
+  bt.println(value, 3);
+
+  if (Serial.available())
   {
     char temp = Serial.read();
-    if(temp == 'a')
+    if (temp == 'a')
       calibrationFactor += 10;
-    else if(temp == 'z')
+    else if (temp == 'z') {
       calibrationFactor -= 10;
-    else if(temp == 't')
-      scale.tare();
+    }
+    else if (temp == 't') {
+      scale.tare(samples);
+      zeroFactor = scale.read_average(samples);
+      Serial.println("Scale tared.");
+    }
   }
+
+  if (bt.available())
+  {
+    char temp = bt.read();
+    if (temp == 'a')
+      calibrationFactor += 10;
+    else if (temp == 'z') {
+      calibrationFactor -= 10;
+    }
+    else if (temp == 't') {
+      scale.tare(samples);
+      zeroFactor = scale.read_average(samples);
+      bt.println("Scale tared.");
+    }
+  }
+
+  // delay(2000);
 }
